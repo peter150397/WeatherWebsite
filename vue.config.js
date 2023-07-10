450115e5bc4a1743573a82b74820f6c2825555b9
Peter <peter150397@gmail.com>
2023-07-10 22:00:27 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/WeatherWedsiteBuild/'
-  : '/'
+  publicPath: '/WeatherWebsite',
+  assetsDir: 'WeatherWebsite',
+  outputDir: "dist"
 })
